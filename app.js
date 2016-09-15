f83fb0896045c457ffc2c7a8a8cbafaba352ad9d
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-09-15 09:24:11 -0500

    socket con't

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = require('express')();
 var	server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var openSockets = new Array();
+var players = new Array();
+
 server.listen(8082);
 
 // ROUTAGE
@@ -11,11 +14,43 @@ app.get('/', function handler(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+//Socket general
 io.sockets.on('connect', function(socket){
 
+	var id = makeId();
 	socket.emit('welcome', 
-		{message:"you are welcome"});
+		{message:"you are welcome", room_id:id});
+
+	players[id] = 0;
+
+	//Socket "room" (une partie de pong)
+	openSockets[id] = io.of('/'+id)
+		.on('connection', function(socket){
+
+			players[id] = players[id] + 1;
+
+			socket.emit('playerNumber', {player_id:players[id]});
+
+			socket.on('update', function(X, Y, playerId){
+
+				console.log(X + " " + Y + " " + playerId);
+
+				socket.broadcast.emit('positionChange', X, Y, playerId);
+			});
+		});
 });
+
+function makeId(){
+	var id = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	for (var i = 0; i < 10; i++) {
+		id += possible.charAt(Math.floor(Math.random() 
+			* possible.length));
+	}
+
+	return id;
+}
 
 //node app.js
 // localhost:8081
